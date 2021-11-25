819530a339dfb73a315b21edf0004f54e37c88fb
candacerubin <candacerubin0@gmail.com>
2021-11-25 01:06:58 -0800

    adds functionality to input; logs todos object in console

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './styles.css';
 
 interface Props {
 	todo: string;
 	setTodo: React.Dispatch<React.SetStateAction<string>>;
+	handleOnSubmit: (e: React.FormEvent) => void;
 }
 
-const InputField = ({ todo, setTodo }: Props) => {
+const InputField = ({ todo, setTodo, handleOnSubmit }: Props) => {
+	const inputRef = useRef<HTMLInputElement>(null);
+
 	return (
-		<form className='input'>
+		<form
+			className='input'
+			onSubmit={(e) => {
+				handleOnSubmit(e);
+				inputRef.current?.blur();
+			}}>
 			<input
 				type='text'
 				value={todo}
 				onChange={(e) => setTodo(e.target.value)}
 				placeholder='Declare an objective'
 				className='input-field'
+				ref={inputRef}
 			/>
 			<button className='input-btn' type='submit'>
 				Accept
